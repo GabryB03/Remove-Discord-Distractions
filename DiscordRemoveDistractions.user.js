0494f9ff3fdb237978d9a6fa6c929bc0f48acc4f
Gabriele Bologna <bolognagabriele2003@gmail.com>
2023-11-14 05:45:21 +0100

    Update DiscordRemoveDistractions.user.js

--- DiscordRemoveDistractions.user.js
+++ DiscordRemoveDistractions.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Discord Remove Distractions
 // @namespace    http://tampermonkey.net/
-// @version      1.4
+// @version      1.5
 // @description  Remove all the Discord distractions to get concentrated on what's really important
 // @author       GabryB03
 // @match        https://www.discord.com/*
@@ -14,10 +14,9 @@
 
 (function()
 {
-    const css = ".privateChannelsHeaderContainer_ddcec6,.title_b7d661,.profilePanel__12596,.guilds__2b93a,.searchBar_e4ea2a,.container__5c7e7,.toolbar__88c63,a[href='/store'],a[href='/shop'],a[href='/channels/@me']{display:none!important;visibility:hidden!important;}";
     const head = document.head || document.getElementsByTagName('head')[0];
     const styleElement = document.createElement('style');
     styleElement.type = 'text/css';
-    styleElement.innerHTML = css;
+    styleElement.innerHTML = ".privateChannelsHeaderContainer_ddcec6,.title_b7d661,.profilePanel__12596,.guilds__2b93a,.searchBar_e4ea2a,.container__5c7e7,.toolbar__88c63,a[href='/store'],a[href='/shop'],a[href='/channels/@me']{display:none!important;visibility:hidden!important;}";
     head.appendChild(styleElement);
 })();
